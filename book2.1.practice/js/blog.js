2d19e3e383e289ce4a8b8379b02870c085621e56
sethoak <sethoak@gmail.com>
2019-08-28 21:58:41 -0500

    well it didn't like line 2 of the js file. Need help

---
+++ book2.1.practice/js/blog.js
@@ -0,0 +1,9 @@
+const seekANDdestroy = document.querySelector(".article_header");
+seekANDdestroy.classList.remove("article_header");
+seekANDdestroy.classList.add("new_header");
+
+const name = "Seth";
+
+const new_header = "Welcome to ${name}'s blog!";
+
+console.log("seekANDdestroy", seekANDdestroy);
