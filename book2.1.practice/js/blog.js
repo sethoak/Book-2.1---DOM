3d6f6cf2ea06c8e9b0673b3e907734f13d5c3689
sethoak <sethoak@gmail.com>
2019-08-29 09:24:28 -0500

    finished the first part

--- book2.1.practice/js/blog.js
+++ book2.1.practice/js/blog.js
@@ -1,9 +1,11 @@
-const seekANDdestroy = document.querySelector(".article_header");
-seekANDdestroy.classList.remove("article_header");
+const seekANDdestroy = document.querySelector(".article__header");
+seekANDdestroy.classList.remove("article__header");
 seekANDdestroy.classList.add("new_header");
 
 const name = "Seth";
 
-const new_header = "Welcome to ${name}'s blog!";
+const replacetext = `Welcome to ${name}'s blog!`;
+
+seekANDdestroy.textContent = replacetext;
 
 console.log("seekANDdestroy", seekANDdestroy);
